3d65c3fa2aeed4f8dadabe711fe200dc765025ae
Ian Cenry <iancenry@gmail.com>
2023-02-18 22:08:23 +0300

    removed some comments

--- src/App.jsx
+++ src/App.jsx
@@ -24,25 +24,3 @@ function App() {
 }
 
 export default App;
-
-
-//alternative  --applies in components without many props
-/*
-<Card 
-            key={item.id}
-            img ={require(`./images/${item.coverImg}`)}   
-            rating = {item.stats.rating}   
-            reviewCount = {item.stats.reviewCount} 
-            location = {item.location}
-            title = {item.title}
-            price = {item.price}
-            openSpots = {item.openSpots}
-    
-    />*/
-
-// the syntax above is exactly similar as the one below but using the spread operator
-/*<Card 
-            key={item.id}
-            {...item}
-    
-    />*/
